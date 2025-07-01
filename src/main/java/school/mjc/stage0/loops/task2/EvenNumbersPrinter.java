683a133dac698fbb59cf2397961d058b88fc032e
KenTaR0-gg <kotya.piso@gmail.com>
2025-07-01 17:14:18 +0300

    module 4. task 2. + Evelina's task in (Even...)

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,5 +2,32 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
+        int i = 0;
+        while (i <= printTillInclusive) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
+
+//    public void printEvenNumbers(int begin, int end) {
+//        int i = begin;
+//        while (i <= end) {
+//            if (i % 2 == 0 && i != 0) {
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        int begin = 23;
+//        int end = 27;
+//        EvenNumbersPrinter evenNumbersPrinter = new EvenNumbersPrinter();
+//        evenNumbersPrinter.printEvenNumbers(23, 27);
+//    }
+//
+//}
